48f2a1a0eb9bc16a31204017b4c15d3373480183
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-03-18 23:29:07 +0530

    Explore access characters in a string using negative indexing

--- strings.py
+++ strings.py
@@ -1,8 +1,19 @@
 
 parrot= "Norwegian Blue"
+print("Positive")
 print(parrot[3].upper())
 print(parrot[4])
-print()
+print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
+print()
+print("Negative")
+print(parrot[-11].upper())
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
+print(len(parrot))
