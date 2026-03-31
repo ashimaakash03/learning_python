4c8a13af3267570de472ccc9fa7d2dcdd4320a81
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-03-31 10:44:35 +0530

    Build a number guessing game using simple if - elif - else conditional statements

--- conditionals.py
+++ conditionals.py
@@ -6,3 +6,14 @@ if age >= 18:
 	print(f"{name} is old enough to vote")
 else:
 	print(f"{name} is not old enough to vote")
+
+#guess the correct number game
+actual_number= 15
+input_number= int(input("Choose a number: "))
+
+if actual_number > input_number:
+	print(f"Your guessed number is greater than the number")
+elif actual_number < input_number:
+	print(f"Your guessed number is less than the number")
+else:
+	print(f"Your got it right...")
