05c8e7d944d454d3f190704889d2054d62196df9
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-04-07 12:07:38 +0530

    Modify code by revising the condition in the while loop and adding an else clause to the loop

--- guessing_game.py
+++ guessing_game.py
@@ -1,28 +1,31 @@
-#Guess the number algorithm using binary search approach
+# Guess the number algorithm using binary search approach
 
 
-low= 1
-high= 1000
+low = 1
+high = 1000
+mid = 0
 
-guesses=0
+guesses = 0
 print(f"Please guess a number between {low} and {high}")
 input("Press enter to continue...")
 
-while low<=high:
-	guesses+=1
-	mid= (low+high)//2
+while low < high:
+	guesses += 1
+	mid = (low + high) // 2
 	print(f"Is your number {mid}")
-	response= input().strip().upper()
+	response = input().strip().upper()
 
-	if response== 'Y':
+	if response == 'Y':
 		print(f"You got it in {guesses} guesses! The number is {mid}")
 		break
-	elif response== 'H':
-		low= mid+1
+	elif response == 'H':
+		low = mid + 1
 		print("Searching upper half...")
-	elif response== 'L':
-		high= mid-1
+	elif response == 'L':
+		high = mid - 1
 		print("Searching lower half...")
 	else:
 		print("You choose to quit the game")
 		break
+else:
+	print(f"You got it in {guesses} guesses! The number is {mid}")
