7319fd503e2c508b086a9bf7fac159adb7515592
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-04-08 23:23:32 +0530

    Slice a list using positive and negative index

--- intro_to_lists.py
+++ intro_to_lists.py
@@ -9,3 +9,5 @@ for part in computer_parts:
 	print(part)
 print()
 print(computer_parts[2]) # Output: keyboard
+print(computer_parts[0:3]) #Output: [computer, monitor, keyboard] (index 0 to 2)
+print(computer_parts[-1]) # Output: mouse pad (index -1 to access last element)
