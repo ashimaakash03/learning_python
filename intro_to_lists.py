9eb05aec83e1115cce6a8b9a3bc36fe61497373c
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-04-08 23:03:40 +0530

    Intro to lists, print a list

---
+++ intro_to_lists.py
@@ -0,0 +1,9 @@
+computer_parts= ["computer",
+                 "monitor",
+                 "keyboard",
+                 "mouse",
+                 "mouse pad"] #lists are created using sq brackets []
+
+#print a list
+for part in computer_parts:
+	print(part)
