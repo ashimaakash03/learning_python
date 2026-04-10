7d844369a28cce959ae9771b1c04626bfaefdb5c
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-04-10 23:02:02 +0530

    Reformat code

--- intro_to_lists.py
+++ intro_to_lists.py
@@ -1,13 +1,16 @@
-computer_parts= ["computer",
-                 "monitor",
-                 "keyboard",
-                 "mouse",
-                 "mouse pad"] #lists are created using sq brackets []
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse pad"]  # lists are created using sq brackets []
 
-#print a list
+# print a list
 for part in computer_parts:
-	print(part)
+	print(part)  # This will print each individual list item in new line
+print(computer_parts)  # This will print all elements in the form of a list
 print()
-print(computer_parts[2]) # Output: keyboard
-print(computer_parts[0:3]) #Output: [computer, monitor, keyboard] (index 0 to 2)
-print(computer_parts[-1]) # Output: mouse pad (index -1 to access last element)
+print(computer_parts[2])  # Output: keyboard
+print(
+	computer_parts[0:3])  # Output: [computer, monitor, keyboard] (index 0
+# to 2)
+print(computer_parts[-1])  # Output: mouse pad (index -1 to access last element)
