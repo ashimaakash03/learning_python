fc4c5f32eae482e403b03bbb61ea812b0ae591fa
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-04-11 10:35:04 +0530

    Explore basic functions in the list (min, max, len, count)

--- intro_to_lists.py
+++ intro_to_lists.py
@@ -14,3 +14,16 @@ print(
 	computer_parts[0:3])  # Output: [computer, monitor, keyboard] (index 0
 # to 2)
 print(computer_parts[-1])  # Output: mouse pad (index -1 to access last element)
+
+# min and max
+numbers = [12, 34, 56, 78, 90, 23, 45, 67, 89]
+print(f"Minimum in the list: {min(numbers)}")
+print(f"Maximum in the list: {max(numbers)}")
+print(f"Number of elements in the list: {len(numbers)}")
+
+# count function
+river = "mississippi"
+print(f"Number of 's': {river.count('s')}")  # 4 's' present in mississippi
+print(f"Number of 'i': {river.count('i')}")  # 4 'i' present in mississippi
+print(
+	f"Number of 'iss': {river.count('iss')}")  # 2 'iss' present in mississippi
