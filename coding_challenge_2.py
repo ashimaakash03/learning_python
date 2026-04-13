c255d36ce71492b99756313f1c58f9d884d8750d
Ashim Aakash Roy <ashimaakash1997@gmail.com>
2026-04-13 20:56:31 +0530

    Build a program to dynamically append an item into a list according to the input by user

---
+++ coding_challenge_2.py
@@ -0,0 +1,36 @@
+# Write a program to append an item to the list according to the user input.
+
+list_items=[]
+
+choice= int(input("Please select the items to add to the list..."))
+
+while choice != 0:
+	if choice == 1:
+		print(f"You selected option {choice}. Adding computer")
+		list_items.append("computer")
+		choice = int(input("What do you wish to add next?"))
+	elif choice == 2:
+		print(f"You selected option {choice}. Adding monitor")
+		list_items.append("monitor")
+		choice = int(input("What do you wish to add next?"))
+	elif choice == 3:
+		print(f"You selected option {choice}. Adding keyboard")
+		list_items.append("keyboard")
+		choice = int(input("What do you wish to add next?"))
+	elif choice == 4:
+		print(f"You selected option {choice}. Adding mouse")
+		list_items.append("mouse")
+		choice = int(input("What do you wish to add next?"))
+	elif choice == 5:
+		print(f"You selected option {choice}. Adding mouse pad")
+		list_items.append("mouse pad")
+		choice = int(input("What do you wish to add next?"))
+	else:
+		print("You selected an invalid option. Please select an option "
+		      "between 1 to 5")
+		choice = int(input("What do you wish to add next?"))
+
+else:
+	print(f"You selected option {choice}. That means you quit")
+	print(f"The list contains: {list_items}")
+
